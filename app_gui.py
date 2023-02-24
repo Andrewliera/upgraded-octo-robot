3d4af812f2773936ed279e800dd870f5da0fc725
andrew <andrewliera15@gmail.com>
2023-02-23 21:58:31 -0500

    added more gui

--- app_gui.py
+++ app_gui.py
@@ -1,53 +1,113 @@
 import sys
-from PySide6 import QtWidgets, QtGui, QtSql
-
-
-class Color(QtWidgets.QWidget):
-
-    def __init__(self, color):
-        super(Color, self).__init__()
-        self.setAutoFillBackground(True)
-
-        palette = self.palette()
-        palette.setColor(QtGui.QPalette.Window, QtGui.QColor(color))
-        self.setPalette(palette)
+from PySide6 import QtWidgets, QtSql
 
 
 class MyGui(QtWidgets.QMainWindow):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, parent=None):
+        super().__init__(parent)
         self.setWindowTitle("Wufoo Form Viewer")
+        # connect to local db
         self.db = QtSql.QSqlDatabase.addDatabase('QSQLITE')
         self.db.setDatabaseName('my_form_db.sqlite')
         self.db.open()
+
+        # configure gui model from db
         self.model = QtSql.QSqlTableModel(self, db=self.db)
         self.model.setTable("entries")
         self.model.select()
         self.table_view = QtWidgets.QTableView()
         self.table_view.setModel(self.model)
+        self.vertical_header = self.table_view.verticalHeader()
+
+        # make form
+        top_layout = QtWidgets.QFormLayout()
+        title_label = QtWidgets.QLabel("Title")
+        f_name_label = QtWidgets.QLabel("First")
+        l_name_label = QtWidgets.QLabel("Last")
+        org_label = QtWidgets.QLabel("org name")
+        email_label = QtWidgets.QLabel("email")
+        org_site_label = QtWidgets.QLabel("org site")
+        phone_label = QtWidgets.QLabel("phone")
+
+        top_layout.addRow(title_label, QtWidgets.QLineEdit())
+        top_layout.addRow(f_name_label, QtWidgets.QLineEdit())
+        top_layout.addRow(l_name_label, QtWidgets.QLineEdit())
+        top_layout.addRow(org_label, QtWidgets.QLineEdit())
+        top_layout.addRow(email_label, QtWidgets.QLineEdit())
+        top_layout.addRow(org_site_label, QtWidgets.QLineEdit())
+        top_layout.addRow(phone_label, QtWidgets.QLineEdit())
+
+        # add checkboxes
+        checkbox_set_one_layout = QtWidgets.QVBoxLayout()
+        proj_checkbox = QtWidgets.QCheckBox("project")
+        guest_checkbox = QtWidgets.QCheckBox("guest")
+        site_visit_checkbox = QtWidgets.QCheckBox("site vist")
+        job_shadow_checkbox = QtWidgets.QCheckBox("job shadow")
+        internship_checkbox = QtWidgets.QCheckBox("internship")
+        networking_checkbox = QtWidgets.QCheckBox("networking")
+
+        checkbox_set_one_layout.addWidget(proj_checkbox)
+        checkbox_set_one_layout.addWidget(guest_checkbox)
+        checkbox_set_one_layout.addWidget(site_visit_checkbox)
+        checkbox_set_one_layout.addWidget(job_shadow_checkbox)
+        checkbox_set_one_layout.addWidget(internship_checkbox)
+        checkbox_set_one_layout.addWidget(networking_checkbox)
+
+        checkbox_set_two_layout = QtWidgets.QVBoxLayout()
+        summer_checkbox = QtWidgets.QCheckBox("summer")
+        spring_checkbox = QtWidgets.QCheckBox("spring")
+        winter_checkbox = QtWidgets.QCheckBox("winter")
+        summer2_checkbox = QtWidgets.QCheckBox("summer2")
+
+        checkbox_set_two_layout.addWidget(summer_checkbox)
+        checkbox_set_two_layout.addWidget(spring_checkbox)
+        checkbox_set_two_layout.addWidget(winter_checkbox)
+        checkbox_set_two_layout.addWidget(summer2_checkbox)
+
+        full_widget = QtWidgets.QWidget()
+        view_entry_layout = QtWidgets.QVBoxLayout()
+        view_entry_layout.addLayout(top_layout)
+        view_entry_layout.addLayout(checkbox_set_one_layout)
+        view_entry_layout.addLayout(checkbox_set_two_layout)
+        full_widget.setLayout(view_entry_layout)
+        # configure main widget from table
+        list_widget = QtWidgets.QListWidget()
+        tab_layout = QtWidgets.QHBoxLayout()
+        tab_layout.addWidget(list_widget, 1)
+        tab_layout.addWidget(full_widget, 2)
+        # tab_layout.addWidget(text_widget, 4)
+
+        view_entry_list = QtWidgets.QWidget()
+        view_entry_list.setLayout(tab_layout)
 
         tabs = QtWidgets.QTabWidget()
         tabs.setTabPosition(QtWidgets.QTabWidget.North)
         tabs.setMovable(True)
         tabs.setDocumentMode(True)
 
+        tabs.addTab(view_entry_list, "View Entries")
         tabs.addTab(self.table_view, "View Database")
-        tabs.addTab(Color("blue"), "blue")
+
+        num_of_entries = self.model.rowCount()
+        column_data = []
+        for column in range(num_of_entries):
+            column_index = self.model.index(column, 0)
+            column_data.append(self.model.data(column_index))
+        # check the data you have is the one *think* it should be
+        for i in range(len(column_data)):
+            list_widget.addItem(str(column_data[i]))
+
         self.setCentralWidget(tabs)
 
 
 def run_gui():
-    app = QtWidgets.QApplication(sys.argv)
+    app = QtWidgets.QApplication()
     window = MyGui()
-    window.resize(200, 100)
-    window.setStyleSheet("""
-        background-color: #262626;
-        color: #FFFFFF;
-        font-family: Arial;
-        font-size: 18px;
-        """)
     window.show()
-    app.exec()
+    with open("styles/style.qss", "r")as f:
+        _style = f.read()
+        app.setStyleSheet(_style)
+    sys.exit(app.exec())
 
 
 run_gui()
--- main.py
+++ main.py
@@ -1,39 +1,12 @@
-import wufoo_db
 import get_wufuu_info
+import wufoo_db
 import app_gui
 
 
-class BadInput(Exception):
-    """Exception raised in user input"""
-    pass
-
-
-def my_main():
+if __name__ == '__name__':
+    entry_file = 'my_entries.json'
+    db_file = 'my_form_db.sqlite'
+    call_api = get_wufuu_info.get_form_info()
+    get_wufuu_info.save_response_as_file(call_api, entry_file)
+    wufoo_db.configure_db(db_file, entry_file)
     app_gui.run_gui()
-
-
-if __name__ == '__main__':
-    print("Welcome to a simple Wufoo form checker")
-    try:
-        db_file = 'my_form_db.sqlite'
-        entry_file = 'my_entries.json'
-        start_prog = input("To continue press c"
-                           "\n other keys to exit"
-                           "\nYour Input: ")
-        while start_prog == 'c':
-            user_input = input("Save your Wufoo Form entries?"
-                               "\ny key to continue"
-                               "\n other keys to exit"
-                               "\nYour Input: ")
-            if user_input == 'y':
-                api_output = get_wufuu_info.get_form_info()
-                get_wufuu_info.save_response_as_file(api_output, entry_file)
-                wufoo_db.configure_db(db_file, entry_file)
-                print("\nFile Entries Saved")
-            else:
-                start_prog = input("To continue press c"
-                                   "\n other keys to exit"
-                                   "\nYour Input: ")
-        print("Exiting Program")
-    except BadInput:
-        raise BadInput()
